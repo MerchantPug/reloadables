47314d1229a61b9a00d638b6713696d0598e1264
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-04 00:19:51 +1100

    Remove unused fields.

--- common/src/main/java/net/merchantpug/reloadables/Reloadables.java
+++ common/src/main/java/net/merchantpug/reloadables/Reloadables.java
@@ -2,14 +2,10 @@ package net.merchantpug.reloadables;
 
 import dev.greenhouseteam.rdpr.api.IReloadableRegistryCreationHelper;
 import net.minecraft.core.registries.Registries;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Reloadables {
 
 	public static final String MOD_ID = "reloadables";
-	public static final String MOD_NAME = "Reloadables";
-	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
 	public static void registerReloadables(IReloadableRegistryCreationHelper helper) {
 		helper.fromExistingRegistry(Registries.CHAT_TYPE);
